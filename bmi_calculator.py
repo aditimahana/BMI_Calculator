69a2f687fb227ba5312d351183a52e82b71bada2
Aditi Mahana <aditi.mahanaofs@gmail.com>
2026-08-09 17:51:14 +0800

    Added Completed Code

---
+++ bmi_calculator.py
@@ -0,0 +1,8 @@
+height = float(input("What is your height?"))
+weight = float(input("What is your weight?"))
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight/(height * height)
+
+print("Here is your BMI: " + str(bmi))
