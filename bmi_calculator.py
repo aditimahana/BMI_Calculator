0a095c3daf4896ec7f2483d9311a09f40290fd91
Aditi Mahana <aditi.mahanaofs@gmail.com>
2026-08-10 17:56:52 +0800

    Add BMI classification messages

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -6,3 +6,11 @@ weight = float(input("What is your weight?"))
 bmi = round(weight/(height * height), 2)
 
 print("Here is your BMI: " + str(bmi))
+
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi < 25:
+    print("You have a normal weight")
+else: 
+    print("You are overweight")
+    
